495a97f149f323d4bee4ea1fb68baf6b9e285516
Jacob Shulenberger <jacob@builtdigital.io>
2022-11-29 16:41:25 -0600

    shaping up load command, get ready for tui

--- src/load.rs
+++ src/load.rs
@@ -1,11 +1,50 @@
-use std::process::Command;
+use clap::ArgMatches;
+use log::info;
+use std::fs;
+use std::io;
+use std::process::{Command, Stdio};
 
-pub fn init() {
-    println!("run load");
+pub fn init(args: &ArgMatches) {
+    info!(
+        "beginning load::  update: {:?} | link: {:?}",
+        args.get_flag("update"),
+        args.get_flag("link")
+    );
+    install_homebrew();
 }
 
 fn install_homebrew() {
-    Command::new("NONINTERACTIVE=1 /bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"")
-    .spawn()
-    .expect("homebrew installation failed to start");
+    let mut homebrew_status = Command::new("which")
+        .arg("dot")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to check homebrew");
+    let stdout = homebrew_status.stdout.take().unwrap();
+    println!("out: {:?}", stdout);
+
+    let which_output = homebrew_status
+        .wait_with_output()
+        .expect("failed in wait for 'which'");
+    if which_output.status.success() {
+        info!("homebrew already installed")
+    } else {
+        info!("installing homebrew");
+
+        let resp = reqwest::blocking::get(
+            "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh",
+        )
+        .expect("request failed");
+        let body = resp.text().expect("body invalid");
+        let mut out = fs::File::create("homebrew_install.sh").expect("failed to create file");
+        io::copy(&mut body.as_bytes(), &mut out).expect("failed to copy content");
+        let install_script = fs::read_to_string("homebrew_install.sh")
+            .expect("Should have been able to read the file");
+
+        Command::new("/bin/bash")
+        .env("NONINTERACTIVE", "1")
+        .arg("-c")
+        .arg(install_script)
+        .spawn()
+        .expect("homebrew installation failed to start");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 use clap::{arg, command, Command};
+use log::{error, info, warn};
+use pretty_env_logger;
 mod load;
 
 fn main() {
+    pretty_env_logger::init();
+
     let matches = command!() // requires `cargo` feature
-    .propagate_version(true)
-    .subcommand_required(true)
-    .arg_required_else_help(true)
-    .subcommand(
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
             Command::new("load")
-            .about("Initializes and updates the system")
-            .arg(arg!(update: -u --update "run updates only")),
-    )
-    .get_matches();
+                .about("Initializes and updates the system")
+                .arg(arg!(update: -u --update "run updates only"))
+                .arg(arg!(link: -s --link "symlink dotfiles")),
+        )
+        .get_matches();
 
     match matches.subcommand() {
-        Some(("load", sub_matches)) => println!(
-                "load called with arg: {:?}",
-            sub_matches.get_flag("update")
-        ),
-        _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
+        Some(("load", sub_matches)) => {
+            load::init(sub_matches);
+        }
+        _ => error!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
-    load::init();
 }
