0ea76c1545123b7a70423b8fd85eb3b63cfde3cd
Jacob Shulenberger <jacob@builtdigital.io>
2024-06-21 21:31:50 -0500

    refactor: restructure commands init

--- cmd/cmds.go
+++ cmd/cmds.go
@@ -8,11 +8,10 @@ import (
 	"github.com/spf13/cobra"
 
 	"hew.jacobshu.dev/pkg/cht"
+	"hew.jacobshu.dev/pkg/hash"
 	"hew.jacobshu.dev/pkg/kinsta"
 	"hew.jacobshu.dev/pkg/load"
 	"hew.jacobshu.dev/pkg/shared"
-	"hew.jacobshu.dev/pkg/stardew"
-  "hew.jacobshu.dev/pkg/hash"
 )
 
 func BuildCmdTree() *cobra.Command {
@@ -20,94 +19,85 @@ func BuildCmdTree() *cobra.Command {
 		Use:   "hew",
 		Short: "A handy haversack with tools ready to hand",
 		Args:  cobra.NoArgs,
-		RunE:  hewRoot,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return cmd.Help()
+		},
 	}
 
-	var chtCmd = &cobra.Command{
-		Use:   "cht",
-		Short: "Get help from cht.sh",
-		Args:  cobra.NoArgs,
-		Run:   chtRoot,
-	}
-	rootCmd.AddCommand(chtCmd)
-
-	var loadCmd = &cobra.Command{
-		Use:   "load",
-		Short: "Configure your system for maximum awesomeness",
-		Args:  cobra.NoArgs,
-		Run:   loadRoot,
-	}
-	rootCmd.AddCommand(loadCmd)
-
-	var kinstaCmd = &cobra.Command{
-		Use:   "kinsta",
-		Short: "Orchestrate Kinsta from the comfort of your terminal",
-		Args:  cobra.NoArgs,
-		Run:   kinstaRoot,
-	}
-	rootCmd.AddCommand(kinstaCmd)
-
-	var stardewCmd = &cobra.Command{
-		Use:   "stardew",
-		Short: "game utils",
-		Args:  cobra.NoArgs,
-		Run:   stardewRoot,
-	}
-	rootCmd.AddCommand(stardewCmd)
-
-	var hashCmd = &cobra.Command{
-		Use:   "hash",
-		Short: "hashing utilities",
-    Args: cobra.MatchAll(cobra.ExactArgs(2), cobra.OnlyValidArgs),
-		Run:   hashRoot,
-	}
-	rootCmd.AddCommand(hashCmd)
+	rootCmd.AddCommand(newChtCmd())
+	rootCmd.AddCommand(newLoadCmd())
+	rootCmd.AddCommand(newKinstaCmd())
+	rootCmd.AddCommand(newHashCmd())
 
 	return rootCmd
 }
 
-func hewRoot(cmd *cobra.Command, args []string) error {
-	return cmd.Help()
-}
-
-func chtRoot(cmd *cobra.Command, args []string) {
-	p := tea.NewProgram(
-		cht.InitialChtModel(),
-		tea.WithAltScreen(),       // use the full size of the terminal in its "alternate screen buffer"
-		tea.WithMouseCellMotion(), // turn on mouse support so we can track the mouse wheel
-	)
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Uh oh, there was an error: %v\n", err)
-		os.Exit(1)
-	}
-}
-
-func kinstaRoot(cmd *cobra.Command, args []string) {
-  // data, err := kinsta.IsOperationFinished("")
-  data, err := kinsta.CreateManualBackup("", "test")
-	if err != nil {
-		fmt.Printf("error\n%#v\n", err)
+func newChtCmd() *cobra.Command {
+	var chtCmd = &cobra.Command{
+		Use:   "cht",
+		Short: "Get help from cht.sh",
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			p := tea.NewProgram(
+				cht.InitialChtModel(),
+				tea.WithAltScreen(),       // use the full size of the terminal in its "alternate screen buffer"
+				tea.WithMouseCellMotion(), // turn on mouse support so we can track the mouse wheel
+			)
+			if _, err := p.Run(); err != nil {
+				fmt.Printf("Uh oh, there was an error: %v\n", err)
+				os.Exit(1)
+			}
+		},
 	}
 
-  shared.Pprint(data) 
+	return chtCmd
 }
 
-func loadRoot(cmd *cobra.Command, args []string) {
-	p := tea.NewProgram(load.NewLoadModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Println("Error running load:", err)
-		os.Exit(1)
+func newLoadCmd() *cobra.Command {
+	var loadCmd = &cobra.Command{
+		Use:   "load",
+		Short: "Configure your system for maximum awesomeness",
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			p := tea.NewProgram(load.NewLoadModel())
+			if _, err := p.Run(); err != nil {
+				fmt.Println("Error running load:", err)
+				os.Exit(1)
+			}
+		},
 	}
+
+	return loadCmd
 }
 
-func stardewRoot(cmd *cobra.Command, args []string) {
-	p := tea.NewProgram(stardew.NewStardewModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Error running stardew: \n%#v", err)
-		os.Exit(1)
+func newKinstaCmd() *cobra.Command {
+	var kinstaCmd = &cobra.Command{
+		Use:   "kinsta",
+		Short: "Orchestrate Kinsta from the comfort of your terminal",
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			// data, err := kinsta.IsOperationFinished("")
+			data, err := kinsta.CreateManualBackup("", "test")
+			if err != nil {
+				fmt.Printf("error\n%#v\n", err)
+			}
+
+			shared.Pprint(data)
+		},
 	}
+
+	return kinstaCmd
 }
 
-func hashRoot(cmd *cobra.Command, args []string) {
-  	
+func newHashCmd() *cobra.Command {
+	var hashCmd = &cobra.Command{
+		Use:   "hash",
+		Short: "hashing utilities",
+		Args:  cobra.MatchAll(cobra.ExactArgs(2), cobra.OnlyValidArgs),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("%v", args)
+		},
+	}
+
+	return hashCmd
 }
