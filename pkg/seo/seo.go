5736eb8d683e22c4220c63c57d31161f81eeaa8c
Jacob Shulenberger <jacob@builtdigital.io>
2024-07-08 13:34:40 -0500

    wip: dataforseo on page stuff

--- pkg/seo/seo.go
+++ pkg/seo/seo.go
@@ -1,54 +1,63 @@
 package seo
 
 import (
-   "bytes"
-   "encoding/json"
-   "io"
-   "log"
-   "net/http"
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"path"
+	"time"
 )
 
-func main() {
-  postArray := []map[string]string
-  postArray = append(postArray, make(map[string]string{
-    "target": "dataforseo.com",
-    "max_crawl_pages": 10,
-    "load_resources": true,
-    "enable_javascript": true,
-    "custom_js": "meta = {}; meta.url = document.URL; meta;",
-    "tag": "some_string_123",
-    "pingback_url": "https://your-server.com/pingscript?id=$id&tag=$tag",
-  }))
-  
-  {
-    method: 'post',
-    url: 'https://api.dataforseo.com/v3/on_page/task_post',
-    auth: {
-      username: 'login',
-      password: 'password'
-    },
-    data: post_array,
-    headers: {
-      'content-type': 'application/json'
-    }
-  }
+func main() {	
+  // results found on 
+	type OnPageOptions struct {
+		Target                  string `json:"target"`
+		MaxCrawledPages         int    `json:"max_crawled_pages"`
+		EnableContentParsing    bool   `json:"enable_content_parsing"`
+		BrowserPreset           string `json:"browser_preset"`
+		EnableJavascript        bool   `json:"enable_javascript"`
+		LoadResources           bool   `json:"load_resources"`
+		EnableBrowserRendering  bool   `json:"enable_browser_rendering"`
+		ValidateMicromarkup     bool   `json:"validate_micromarkup"`
+		CalculateKeywordDensity bool   `json:"calculate_keyword_density"`
 
-   postBody, _ := json.Marshal(map[string]string{
-      "name":  "Toby",
-      "email": "Toby@example.com",
-   })
-   responseBody := bytes.NewBuffer(postBody)
-  
-   resp, err := http.Post("https://postman-echo.com/post", "application/json", responseBody)
-   if err != nil {
-      log.Fatalf("An Error Occured %v", err)
-   }
-   defer resp.Body.Close()
+	}
 
-   body, err := io.ReadAll(resp.Body)
-   if err != nil {
-      log.Fatalln(err)
-   }
-   sb := string(body)
-   log.Printf(sb)
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	postBody, _ := json.Marshal(map[string]string{
+		"name":  "Toby",
+		"email": "Toby@example.com",
+	})
+	responseBody := bytes.NewBuffer(postBody)
+
+	req, err := createRequest(http.MethodPost, "/on_page/task_post", postBody)
+	if err != nil {
+		log.Fatalf("an error occurred while creating the request %v", err)
+	}
+	resp, err := client.Do(req)
+	resp, err := http.Post("https://postman-echo.com/post", "application/json", responseBody)
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sb := string(body)
+	log.Printf(sb)
+}
+
+func createRequest(method string, endpoint string, body []byte) (*http.Request, error) {
+	req, err := http.NewRequest(method, path.Join("https://api.dataforseo.com/v3", endpoint), body)
+	if err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth("login", "password")
+	req.Header.Set("Content-Type", "application/json")
+
+	return req, nil
 }
